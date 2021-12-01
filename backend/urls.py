65052509363354737126134cffaca04a81913601
Regis Santos <regis42santos@gmail.com>
2021-11-30 23:29:35 -0300

    Edit backend/urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path('', include('backend.core.urls', namespace='core')),
+    path('todo/', include('backend.todo.urls', namespace='todo')),
     path('admin/', admin.site.urls),
 ]
