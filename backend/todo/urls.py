20eafc792f4274234b03781eb88383a81c77124b
Regis Santos <regis42santos@gmail.com>
2021-11-30 23:37:44 -0300

    Edit backend/todo/urls.py

--- backend/todo/urls.py
+++ backend/todo/urls.py
@@ -1,11 +1,13 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from backend.todo.views import TodoViewSet
+from backend.todo.api.viewsets import TodoViewSet
+
+app_name = 'todo'
 
 router = routers.DefaultRouter()
 
-router.register(r'todoes', TodoViewSet)
+router.register(r'todos', TodoViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
